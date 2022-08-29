d71ae20cdbc1a8766f5b9775b5263dacb223e1e6
Rich Davis <richard.ian.davis+git@gmail.com>
2022-08-29 14:51:20 -0400

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//Rich Davis; 8/29/22; Project 1
+
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string name= "something";
+	std::cout << "Hi, I'm Rich. What's your name?";
+	std::cin >> name;
+	std::cout << "Awesome name, " << name << "!!";
+
+	return 0;
+}
